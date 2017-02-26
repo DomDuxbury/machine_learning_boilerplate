f7e85dd8c4ae60fb2fcc52fe20b040d98db4dae7
DomDuxbury <domduxbury@hotmail.com>
2017-02-26 18:56:59 +0000

    Reformatted preprocessing code

--- examples/preprocess.py
+++ examples/preprocess.py
@@ -3,13 +3,27 @@ from sklearn.preprocessing import StandardScaler, LabelBinarizer
 from sklearn_pandas import DataFrameMapper
 
 def main():
+    df = preprocess_data()
+    print df
+
+def preprocess_data():
+    df = _read_data()
+    df = _engineer_features(df)
+    df = _vectorise_and_scale(df)
+    return df
+
+def _read_data():
     # Read data in and drop incomplete rows
     df = pd.read_csv('./data/titanic.csv')
     df = df.dropna()
+    return df
 
+def _engineer_features(df):
     # Example of engineering a feature from existing
     df['FamilySize'] = df['SibSp'] + df['Parch'] + 1
+    return df
 
+def _vectorise_and_scale(df):
     # Process features 
     # Vectorise labels
     # Scale continuous values 
@@ -23,8 +37,7 @@ def main():
     ], df_out = True)
 
     # Apply the transform
-    df = mapper.fit_transform(df) 
-    print df
+    return mapper.fit_transform(df)
 
 if __name__ == "__main__":
     main()
